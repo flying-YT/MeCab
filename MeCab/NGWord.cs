f2b80d8645baaafe39e3deaca46287e06ed9f731
飛田 優輝 <flying59@outlook.jp>
2021-04-29 14:08:12 +0900

    20210429 commit

---
+++ MeCab/NGWord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MeCab
+{
+    public class NGWord
+    {
+        readonly List<string> ngWordList = new List<string>();
+
+        public NGWord()
+        {
+            SetList();
+        }
+
+        private void SetList()
+        {
+            ngWordList.Add("人");
+            ngWordList.Add("年");
+            ngWordList.Add("日");
+            ngWordList.Add("者");
+            ngWordList.Add("の");
+            ngWordList.Add("こと");
+        }
+
+        public bool CheckNG(string str)
+        {
+            bool b = false;
+            foreach(string ngStr in ngWordList)
+            {
+                if (str == ngStr)
+                {
+                    b = true;
+                    break;
+                }
+            }
+            if (Regex.IsMatch(str, @"^[\d]+$"))
+            {
+                b = true;
+            }
+            return b;
+        }
+    }
+}
--- MeCab/Program.cs
+++ MeCab/Program.cs
@@ -22,6 +22,7 @@ namespace MeCab
                 }
             }
             WriteData(nounList);
+            Aggregate(nounList);
         }
 
 
@@ -67,5 +68,75 @@ namespace MeCab
             }
             sw.Close();
         }
+
+        private static void Aggregate(List<string> _nounList)
+        {
+            List<AggregateData> _list = new List<AggregateData>();
+
+            foreach(string _noun in _nounList)
+            {
+                string[] strArray = _noun.Split(",");
+                foreach(string str in strArray)
+                {
+                    int count = 0;
+                    bool judge = true;
+                    foreach (AggregateData aggregateData in _list)
+                    {
+                        //                        if (aggregateData.Word == _noun)
+                        if (aggregateData.Word == str)
+                        {
+                            judge = false;
+                            break;
+                        }
+                        count++;
+                    }
+//                    Console.WriteLine("str:" + str);
+                    if (judge)
+                    {
+                        //                        _list.Add(new AggregateData { Word = _noun, Count = 1 });
+                        _list.Add(new AggregateData { Word = str, Count = 1 });
+                    }
+                    else
+                    {
+                        AggregateData _aggregateData = _list[count];
+                        _list.RemoveAt(count);
+                        _list.Add(new AggregateData { Word = _aggregateData.Word, Count = _aggregateData.Count + 1 });
+                    }
+                }
+                //Console.ReadKey();
+            }
+
+            NGWord NG = new NGWord();
+            for(int i=0;i<5;i++)
+            {
+                int max = 0;
+                int maxIndex = 0;
+                int count = 0;
+                foreach (AggregateData _aggregateData in _list)
+                {
+                    if (max < _aggregateData.Count)
+                    {
+                        max = _aggregateData.Count;
+                        maxIndex = count;
+                    }
+                    count++;
+                }
+                if (NG.CheckNG(_list[maxIndex].Word))
+                {
+                    i--;
+                }
+                else
+                {
+                    Console.WriteLine("名詞：" + _list[maxIndex].Word + " 回数：" + _list[maxIndex].Count);
+                }
+                _list.RemoveAt(maxIndex);
+            }
+        }
+    }
+
+    public struct AggregateData
+    {
+        public string Word;
+        public int Count;
     }
 }
